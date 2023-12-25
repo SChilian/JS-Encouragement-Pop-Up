0a1c9d049e921203e1168369d61d96a73b205ae8
chilian_s <sierrachilian@gmail.com>
2023-12-24 22:06:11 -0500

    Added functions and refactor Js code

--- js/script.js
+++ js/script.js
@@ -3,11 +3,29 @@ var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
 button.addEventListener("click", function () {
-  modal.classList.add("show-modal");
+  openModal();
   button.innerText = "You've got this!!!";
 });
 
 modalX.addEventListener("click", function () {
-  modal.classList.remove("show-modal");
+  closeModal();
   button.innerText = "Who's Got This?";
 });
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "Escape") {
+    closeModal();
+}
+});
+
+var closeModal = function () {
+  if (modal.classList.contains("show-modal")) {
+    modal.classList.remove("show-modal")
+  }
+};
+
+var openModal = function () {
+  if (modal.classList.add("show-modal")) {
+    button.innerText = "You've got this!!!";
+  }
+}
